bca5963f438ab2a422f4ea36e243dbb7f4af1ca6
bulichova <bulichova84@gmail.com>
2021-08-11 00:07:13 +0300

    state

---
+++ src/redux/actions/courses.js
@@ -0,0 +1,2 @@
+import { createAction } from '@reduxjs/toolkit'
+export const getCoursesList = createAction('GET_COURSES_LIST')
---
+++ src/redux/reducers/courses.js
@@ -0,0 +1,3 @@
+import { createReducer } from '@reduxjs/toolkit'
+const initState = []
+export const coursesReducer = createReducer(initState, {})
